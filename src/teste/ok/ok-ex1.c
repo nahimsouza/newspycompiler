d7a7bcd79cde10bde0ace02859753c416cd29c8a
Fernando Alves <fernando.alves@betalabs.com.br>
2014-04-04 01:53:51 -0300

    ajuste

---
+++ src/teste/ok/ok-ex1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+// Define o tipo Func que eh um ponteiro para funcao 
+typedef void (*Func)();
+ 
+int main() {
+   printf(   "    %s   "   ,    ""   );
+   printf(   "    %s   "   ,    "ok-Ger02"   );
+   printf(   "    %s   "   ,    "The output should be : "   );
+   printf(   "    %s   "   ,    "6 1 2 3 4 5 6 5.00 5.50"   );
+   _a   =   _A   (   )   ;    
+   _a   ->vt[   _m   ]   (   _a   )   ;    
+    
+   return 0;
+}
